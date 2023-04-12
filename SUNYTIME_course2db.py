881b5c01353fbab1b0fe6f582fbcc5097d1d59dd
Minsoo <markkim1@naver.com>
2023-04-12 21:50:40 +0900

    UPDATE - change the format of data

--- SUNYTIME_course2db.py
+++ SUNYTIME_course2db.py
@@ -6,18 +6,18 @@ import pandas as pd
 import course2db
 
 # 1. Input your MongdoDB's USERNAME and PASSWORD
-converter = course2db.Converter(url='DB_ADDRESS')
+converter = course2db.Converter(url='YOUR_DB_ADDRESS')
 
 # 2. Set the client and make a db and a collection
 converter.set_collection(db_name='DB_NAME', collection_name='COLLECTION_NAME', semester='SEMESTER')
 
 # 3. convert the pdf file to make a table - Please write the path of the course list pdf file
-converter.pdf2csv(file_url='PDF_PATH')
+converter.pdf2csv(file_url='PDF_FILE_PATH')
 
 # 4. Add course info to your database 
 # reset_db is adding course info after reset the existing db
 # Warning - if you choose 'reset_db' parameter to True, the likes and reviews part will be reset!
-converter.courses2db(reset_db=False) 
+converter.courses2db(reset_db=True) 
 
 
 
--- course2db.py
+++ course2db.py
@@ -27,7 +27,7 @@ class Converter:
         tabula.convert_into(file_url, "output.csv", output_format="csv", pages='all')
         self.df = pd.read_csv('./output.csv')
         self.category = self.df.columns
-        self.names = ['classNbr', 'subj', 'crs', 'courseTitle', 'sbc', 'cmp', 'sctn', 'credits', 'day', 'startTime', 'endTime', 'room', 'instructor', 'likes', 'reviews', 'instructor_names']
+        self.names = ['classNbr', 'subj', 'crs', 'courseTitle', 'sbc', 'cmp', 'sctn', 'credits', 'day', 'startTime', 'endTime', 'room', 'instructor', 'likes', 'reviews', 'instructor_names', 'semesters']
     
     def courses2db(self, reset_db=True): 
         if (reset_db):
@@ -43,12 +43,14 @@ class Converter:
             
             # 수업 이미 있을 경우
             if find_it != None and self.df[self.category[1]].iloc[i] != 'Subj':
-                find_it[self.names[8]].append({self.semester: self.df[self.category[8]].iloc[i]})
-                find_it[self.names[9]].append({self.semester: self.df[self.category[9]].iloc[i]})
-                find_it[self.names[10]].append({self.semester: self.df[self.category[10]].iloc[i]})
-                find_it[self.names[11]].append({self.semester: self.df[self.category[11]].iloc[i]})
-                find_it[self.names[12]].append({self.semester: self.df[self.category[12]].iloc[i]})
+                find_it[self.names[8]].append(self.df[self.category[8]].iloc[i])
+                find_it[self.names[9]].append(self.df[self.category[9]].iloc[i])
+                find_it[self.names[10]].append(self.df[self.category[10]].iloc[i])
+                find_it[self.names[11]].append(self.df[self.category[11]].iloc[i])
+                find_it[self.names[12]].append(self.df[self.category[12]].iloc[i])
                 find_it[self.names[15]] += ', ' + str(self.df[self.category[12]].iloc[i])
+                find_it[self.names[16]].append(self.semester)
+
 
                 self.collection.find_one_and_update({self.names[1]: self.df[self.category[1]].iloc[i],
                                             self.names[2]: self.df[self.category[2]].iloc[i],
@@ -61,7 +63,8 @@ class Converter:
                                             self.names[10]: find_it[self.names[10]],
                                             self.names[11]: find_it[self.names[11]],
                                             self.names[12]: find_it[self.names[12]],
-                                            self.names[15]: find_it[self.names[15]]}
+                                            self.names[15]: find_it[self.names[15]],
+                                            self.names[16]: find_it[self.names[16]]}
                                             })
                 print("Exsiting course modified! : " + str(find_it[self.names[3]]))
 
@@ -76,14 +79,16 @@ class Converter:
                 self.names[5]: self.df[self.category[5]].iloc[i],
                 self.names[6]: self.df[self.category[6]].iloc[i],
                 self.names[7]: self.df[self.category[7]].iloc[i],
-                self.names[8]: [{self.semester: self.df[self.category[8]].iloc[i]}],
-                self.names[9]: [{self.semester: self.df[self.category[9]].iloc[i]}],
-                self.names[10]: [{self.semester: self.df[self.category[10]].iloc[i]}],
-                self.names[11]: [{self.semester: self.df[self.category[11]].iloc[i]}],
-                self.names[12]: [{self.semester: self.df[self.category[12]].iloc[i]}],
+                self.names[8]: [self.df[self.category[8]].iloc[i]],
+                self.names[9]: [self.df[self.category[9]].iloc[i]],
+                self.names[10]: [self.df[self.category[10]].iloc[i]],
+                self.names[11]: [self.df[self.category[11]].iloc[i]],
+                self.names[12]: [self.df[self.category[12]].iloc[i]],
                 self.names[13]: [],
                 self.names[14]: [],
-                self.names[15]: str(self.df[self.category[12]].iloc[i])
+                self.names[15]: str(self.df[self.category[12]].iloc[i]),
+                self.names[16]: [self.semester]
+
                 })
                 print("New course added! : " + str(self.df[self.category[3]].iloc[i]))
    
